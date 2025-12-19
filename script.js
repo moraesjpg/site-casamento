094a62cfbe44674d0dd2f04c81469b5ca6796660
moraesjpg <105519381+moraesjpg@users.noreply.github.com>
2025-12-19 19:46:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<section class="countdown">
+  <h2>Contagem regressiva</h2>
+  <div id="timer">
+    <div><span id="days">0</span><small>Dias</small></div>
+    <div><span id="hours">0</span><small>Horas</small></div>
+    <div><span id="minutes">0</span><small>Minutos</small></div>
+    <div><span id="seconds">0</span><small>Segundos</small></div>
+  </div>
+</section>
